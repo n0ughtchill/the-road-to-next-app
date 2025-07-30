a021e36eec57d4a7e85dc63a96aa5c7a4f6cc772
Brian Stempien <brian.stempien@cat.com>
2025-07-30 16:56:49 -0500

    end of 7/30

---
+++ src/app/tickets/page.tsx
@@ -0,0 +1,45 @@
+import clsx from "clsx";
+import Link from "next/link";
+import { initialTickets } from "@/data";
+import { ticketPath } from "@/paths";
+
+const TICKET_ICONS = {
+  OPEN: "0",
+  DONE: "X",
+  IN_PROGRESS: ">",
+};
+
+const TicketsPage = () => {
+  return (
+    <div>
+      <div className="flex-1 flex flex-col gap-y-8">
+        <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+        <p className="text-sm text-muted-foreground">All your tickets</p>
+      </div>
+      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
+        {initialTickets.map((ticket) => (
+          <div
+            key={ticket.id}
+            className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
+          >
+            <div>{TICKET_ICONS[ticket.status]}</div>
+            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+            <p
+              className={clsx("text-sm text-slate-500 truncate", {
+                "line-through": ticket.status === "DONE",
+              })}
+            >
+              {ticket.content}
+            </p>
+
+            <Link href={ticketPath(ticket.id)} className="text-sm underline">
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TicketsPage;
