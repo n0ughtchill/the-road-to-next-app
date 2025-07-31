16ffe557ffe82134e132d223403c7c3bcb96906a
Brian Stempien <brian.stempien@cat.com>
2025-07-31 13:40:54 -0500

    next themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
-import { homePath,ticketsPath } from "@/paths";
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,25 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav
-          className="
-          supports-backdrop-blur:bg-background/60
-          fixed left-0 right-0 top-0 z-20
-          border-b bg-background/95 backdrop-blur
-          w-full flex py-2.5 px-5 justify-between
-          "
-        >
-          <div>
-            <Link href={homePath()} className="text-lg font-bold">
-              Home
-            </Link>
-          </div>
-          <div>
-            <Link href={ticketsPath()} className="text-sm underline">
-              Tickets
-            </Link>
-          </div>
-        </nav>
+        <Header />
         <main
           className="
           min-h-screen flex-1
