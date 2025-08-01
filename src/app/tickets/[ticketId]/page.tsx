0a9526fed0dce319605a689640ccc2100f368c17
Brian Stempien <brian.stempien@cat.com>
2025-08-01 12:11:30 -0500

    finish server components

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,19 +1,18 @@
 import Link from "next/link";
 import { Placeholder } from "@/components/placeholder";
 import { Button } from "@/components/ui/button";
-import { initialTickets } from "@/data";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { getTicket } from "@/features/ticket/queries/get-ticket";
 import { ticketsPath } from "@/paths";
 
 type TicketPageProps = {
-  params: Promise<{
+  params: {
     ticketId: string;
-  }>;
+  };
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-  const { ticketId } = await params;
-  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+  const ticket = await getTicket(params.ticketId);
 
   if (!ticket) {
     return (
--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ticketPath } from "@/paths";
-import { TICKET_ICONS } from "../constants";
-import { Ticket } from "../types";
+import { TICKET_ICONS } from "../../constants";
+import { Ticket } from "../../types";
 
 type TicketItemProps = {
   ticket: Ticket;
