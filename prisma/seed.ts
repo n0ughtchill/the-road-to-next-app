ab4d387dcd97f0c64405e7a6b32cd8e887e52f00
Brian Stempien <brian.stempien@cat.com>
2025-08-01 14:01:11 -0500

    seeded database

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@/generated/prisma/client";
+
+const prisma = new PrismaClient();
+
+const tickets = [
+  {
+    title: "ticket 1",
+    content: "This is the first ticket from the database",
+    status: "DONE" as const,
+  },
+  {
+    title: "ticket 2",
+    content: "This is the second ticket from the database",
+    status: "OPEN" as const,
+  },
+  {
+    title: "ticket 3",
+    content: "This is the third ticket database",
+    status: "IN_PROGRESS" as const,
+  },
+];
+
+const seed = async () => {
+  const t0 = performance.now();
+  console.log("DB Seed: Started...");
+
+  await prisma.ticket.deleteMany();
+
+  await prisma.ticket.createMany({
+    data: tickets,
+  });
+  const t1 = performance.now();
+  console.log(`DB Seed: Finished (${t1 - t0}ms)`);
+};
+
+seed();
