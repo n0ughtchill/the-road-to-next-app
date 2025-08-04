d7dacbe2618839f6fb84634aa4a05bb0a7647a2b
Brian Stempien <brian.stempien@cat.com>
2025-08-04 10:31:41 -0500

    redirect

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -9,7 +9,8 @@ type TicketPageProps = {
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-  const ticket = await getTicket(params.ticketId);
+  const { ticketId } = params;
+  const ticket = await getTicket(ticketId);
 
   if (!ticket) {
     notFound();
