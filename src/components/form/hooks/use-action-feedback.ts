9604883ef25acd63acafbda325dde0f2a722c11f
Brian Stempien <brian.stempien@cat.com>
2025-08-05 12:50:02 -0500

    fixed error bug

--- src/components/form/hooks/use-action-feedback.ts
+++ src/components/form/hooks/use-action-feedback.ts
@@ -1,5 +1,5 @@
 import { ActionState } from "./../utils/to-action-state";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 type OnArgs = {
   actionState: ActionState;
@@ -14,7 +14,11 @@ const useActionFeedback = (
   actionState: ActionState,
   options: UseActionFeedbackOptions
 ) => {
+  const prevTimestamp = useRef(actionState.timestamp);
+  const isUpdate = prevTimestamp.current !== actionState.timestamp;
   useEffect(() => {
+    if (!isUpdate) return;
+
     if (actionState.status === "SUCCESS") {
       options.onSuccess?.({ actionState });
     }
@@ -22,7 +26,9 @@ const useActionFeedback = (
     if (actionState.status === "ERROR") {
       options.onError?.({ actionState });
     }
-  }, [actionState, options]);
+
+    prevTimestamp.current = actionState.timestamp;
+  }, [isUpdate, actionState, options]);
 };
 
 export { useActionFeedback };
